2a836e4411f85643f7e7cb0d64107ff1a95dfe8f
Eeps30 <eeps30@gmail.com>
2018-06-17 16:12:04 -0700

    trying a fix for the 500 error

--- phpMailer/php_mailer/mail_handler.php
+++ phpMailer/php_mailer/mail_handler.php
@@ -45,7 +45,7 @@ if($output['success'] !== null){
 }
 
 $mail = new PHPMailer;
-$mail->SMTPDebug = 3;           // Enable verbose debug output. Change to 0 to disable debugging output.
+$mail->SMTPDebug = 0;           // Enable verbose debug output. Change to 0 to disable debugging output.
 
 $mail->isSMTP();                // Set mailer to use SMTP.
 $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
@@ -89,5 +89,6 @@ if(!$mail->send()) {
     $output['success'] = true;
 }
 echo json_encode($output);
+return true;
 ?>
 
