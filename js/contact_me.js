283053fd9904e04f1641d21a344cf256fbc62ffc
Eeps30 <eeps30@gmail.com>
2018-06-18 13:35:32 -0700

    trying hack

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,11 +1,4 @@
 $(function() {
-
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-    preventSubmit: true,
-    submitError: function($form, event, errors) {
-      // additional error messages or events
-    },
-    submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
       var name = $("input#name").val();
@@ -58,18 +51,14 @@ $(function() {
         }
       });
     },
-    filter: function() {
-      return $(this).is(":visible");
-    },
-  });
+    
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
-    e.preventDefault();
-    $(this).tab("show");
-  });
-});
+  // $("a[data-toggle=\"tab\"]").click(function(e) {
+  //   e.preventDefault();
+  //   $(this).tab("show");
+  // });
 
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#success').html('');
-});
+// /*When clicking on Full hide fail/success boxes */
+// $('#name').focus(function() {
+//   $('#success').html('');
+// });
