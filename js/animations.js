15f604f94d4bbdeaf5e84d5498de111da2e58cc3
Eeps30 <eeps30@gmail.com>
2018-06-21 17:12:18 -0700

    added media queries for about me section

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,44 @@
+$('a').removeClass('active');
 $(document).ready(function(){
-
     $("section").mouseenter(function(){
          var id = $(this).attr('id');
          $('a').removeClass('active');
          $("a[href*="+id+"]").addClass('active');
     });
- 
  });
+
+$(".navIcon").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#homeSection").offset().top},
+        'slow');
+});
+
+$(".aboutMeButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#aboutMeSection").offset().top},
+        'slow');
+});
+
+$(".profileNav").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#aboutMeSection").offset().top},
+        'slow');
+});
+
+$(".portfolioNav").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#portfolioSection").offset().top},
+        'slow');
+});
+
+$(".techSkillsNav").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#techSkillsSection").offset().top},
+        'slow');
+});
+
+$(".contactNav").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contactSection").offset().top},
+        'slow');
+});
